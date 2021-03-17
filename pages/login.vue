b081043b049c026d762a91bc051d11a26abc5c6b
Khalid <sudjud@bk.ru>
2021-03-17 20:12:40 +0300

    Рабочий вход в login.vue

--- pages/login.vue
+++ pages/login.vue
@@ -1,28 +1,53 @@
 <template>
   <div>
     <div class="container">
-      <form @submit.prevent="onSubmit">
+      <el-form :model="loginForm">
+      <el-form-item prop="text">
         <h1>Login</h1>
-        <div class="form-group">
-          <input type="text" class="form-control">
-        </div>
-
-        <div class="form-group">
-          <input type="password" class="form-control">
-        </div>
-      <button class="btn btn-primary" type="submit">Login</button>
-      </form>
-      <nuxt-link to='/'>Go to home</nuxt-link>
-    </div>
+        <el-input
+          type="email"
+          v-model="loginForm.email"
+          autocomplete="off"
+          placeholder="Email"
+        ></el-input>
+      </el-form-item>
+      <el-form-item prop="pass">
+        <el-input
+          type="password"
+          v-model="loginForm.pass"
+          placeholder="Password"
+          autocomplete="off"
+        ></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit()">Login</el-button>
+      </el-form-item>
+    </el-form>
+    <nuxt-link to='/'>Go to home</nuxt-link>
+  </div>
   </div>
 </template>
 
 <script>
+import firebase from 'firebase/app'
+import 'firebase/auth'
 export default {
+  data() {
+    return {
+      loginForm: {
+        email: "",
+        pass: ""
+      }
+    }
+  },
   layout: 'empty',
   methods: {
-    onSubmit(){
-      this.$store.dispatch('login')
+    async onSubmit(){
+      const formData = {
+        email: this.loginForm.email,
+        pass: this.loginForm.pass
+      }
+      await this.$fire.auth.signInWithEmailAndPassword(this.loginForm.email, this.loginForm.pass)
       this.$router.push('/')
     }
   }
@@ -30,9 +55,6 @@ export default {
 </script>
 
 <style lang="sass" scoped>
-
-  
-
   form
     width: 600px
 
--- store/index.js
+++ store/index.js
@@ -3,21 +3,10 @@ import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/database'
 
-firebase.initializeApp({
-  apiKey: "AIzaSyApbm-u73qgWEG5QptDsaTzCFDIFRjNBkw",
-  authDomain: "toolshop-14931.firebaseapp.com",
-  databaseURL: "https://toolshop-14931-default-rtdb.firebaseio.com",
-  projectId: "toolshop-14931",
-  storageBucket: "toolshop-14931.appspot.com",
-  messagingSenderId: "44419510964",
-  appId: "1:44419510964:web:b8800270fa1f5003995de1",
-  measurementId: "G-B8NE7VREY3"
-})
-
 export const actions = {
-  async login({commit, dispatch}, {login, pass}){
+  async login({commit, dispatch}, {email, pass}){
     try {
-      await this.$fire.auth().signInWithEmailAndPassword(login, pass)
+      await this.$fire.auth().signInWithEmailAndPassword(email, pass)
     } catch (e) {
       console.log(e)
       throw e
